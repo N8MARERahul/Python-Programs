c786285c6ca8fd779bae5c055d7de2b120fb8e0f
N8MARERahul <pal521843@gmail.com>
2022-10-08 13:49:48 +0530

    WhatsApp auto type

--- typing.py
+++ typing.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 time.sleep(2)
 count = 1
-while count <= 2:
-    pyautogui.typewrite("Hahahahaha")
+while count <= 20:
+    pyautogui.typewrite("bokachele")
     pyautogui.press("enter")
     count += 1
